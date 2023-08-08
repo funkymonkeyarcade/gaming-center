2aacb6698f160f525fce196301c51f8a3c4d0a35
clementus360 <clementus360@gmail.com>
2023-08-09 00:00:53 +0200

    add social icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
+
+import facebook from "../assets/icons/icons8-facebook.svg"
+import instagram from "../assets/icons/icons8-instagram.svg"
+import linkedin from "../assets/icons/icons8-linkedin.svg"
+import twitter from "../assets/icons/icons8-twitter.svg"
+import youtube from "../assets/icons/icons8-youtube.svg"
+
 export function Footer() {
 	return(
 	  <footer className='flex flex-col gap-4 w-full justify-between py-8 px-12 text-white bg-primary'>
 		<div className='flex justify-between w-full py-4 border-accent border-b-2'>
 		  <div>Logo</div>
-		  <div>socials</div>
+		  <ul className="flex gap-4">
+			<li className="w-10 p-2 rounded-full border-[0.1rem] border-white"><Image src={facebook} width={'50%'}></Image></li>
+			<li className="w-10 p-2 rounded-full border-[0.1rem] border-white"><Image src={twitter} width={'50%'}></Image></li>
+			<li className="w-10 p-2 rounded-full border-[0.1rem] border-white"><Image src={youtube} width={'50%'}></Image></li>
+			<li className="w-10 p-2 rounded-full border-[0.1rem] border-white"><Image src={linkedin} width={'50%'}></Image></li>
+			<li className="w-10 p-2 rounded-full border-[0.1rem] border-white"><Image src={instagram} width={'50%'}></Image></li>
+
+		  </ul>
 		</div>
 		<div>
 		  <ul className='flex gap-8 font-LogikBold text-2xl'>
