215f3a3467480e7aa3b0715fc82915e7713946b3
clementus360 <clementus360@gmail.com>
2023-08-19 19:56:54 +0200

    remove unused images

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ function Hero() {
         <h1 className='text-4xl font-black font-LogikBold'>FIRST E-SPORT CENTER <br/>IN <span className='text-accent'>R</span>WANDA</h1>
         <Link href='/events'><button className='p-4 w-max font-bold bg-accent text-white hover:bg-white hover:text-accent transition-all rounded-md'>UPCOMING EVENTS</button></Link>
       </div>
-      <div className='absolute hidden sm:block w-full sm:h-full overflow-hidden'>
+      <div className='absolute w-full sm:h-full overflow-hidden'>
         <div className='absolute top-0 h-[200%] w-full bg-gradient-to-t from-primary via-primary to-transparent '></div>
         <video autoPlay loop>
           <source src='https://firebasestorage.googleapis.com/v0/b/gaming-906ed.appspot.com/o/pexels_videos_1957727%20(1080p).mp4?alt=media&token=88425fb4-8328-4b0c-b50b-939d93e10e38' />
