b54b9bd88b34b3c4de84667dfaad1c4d12b84e56
Clement <axia@Clements-MacBook-Pro.local>
2023-08-30 23:25:47 +0200

    update meta-tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,10 +16,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta property="og:title" content="eGamers.rw - Funky Monkey Arcade" />
-        <meta property="og:description" content="Explore the ultimate gaming experience at Funky Monkey Arcade in Rwanda." />
+        <meta property="og:url" content="https://www.egamers.rw" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="eGamers.rw - Funky Monkey Arcade in Mundi Center Rwandex," />
+        <meta property="og:description" content="Welcome to eGamers.rw! Explore the ultimate gaming experience at Funky Monkey Arcade, located in Mundi Center Rwandex, Rwanda. Enjoy a wide range of arcade games and entertainment for all ages. Join us for a fun-filled adventure in the heart of Rwanda's gaming scene." />
         <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/gaming-906ed.appspot.com/o/image.jpg?alt=media&token=04f8cca6-e7bb-4e30-8249-f4d89140ccdf" />
-        <meta property="og:url" content="https://egamers.rw/about" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="egamers.rw" />
+        <meta property="twitter:url" content="https://www.egamers.rw" />
+        <meta name="twitter:title" content="eGamers.rw - Funky Monkey Arcade in Mundi Center Rwandex," />
+        <meta name="twitter:description" content="Welcome to eGamers.rw! Explore the ultimate gaming experience at Funky Monkey Arcade, located in Mundi Center Rwandex, Rwanda. Enjoy a wide range of arcade games and entertainment for all ages. Join us for a fun-filled adventure in the heart of Rwanda's gaming scene." />
+        <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/gaming-906ed.appspot.com/o/image.jpg?alt=media&token=04f8cca6-e7bb-4e30-8249-f4d89140ccdf" />
 
         <link rel="icon" href="/favicon.ico" type="image/x-icon"></link>
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"></link>
