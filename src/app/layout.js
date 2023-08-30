04c77eec4e06cacb9ea36c13db1e87b5dafa337f
Clement <axia@Clements-MacBook-Pro.local>
2023-08-30 23:56:57 +0200

    update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,29 +9,28 @@ export const metadata = {
   title: 'eGamers.rw - Funky Monkey Arcade in Mundi Center Rwandex,',
   keywords: 'eGamers, Funky Monkey Arcade, arcade games, entertainment, Mundi Center Rwandex, Rwanda',
   description: "Welcome to eGamers.rw! Explore the ultimate gaming experience at Funky Monkey Arcade, located in Mundi Center Rwandex, Rwanda. Enjoy a wide range of arcade games and entertainment for all ages. Join us for a fun-filled adventure in the heart of Rwanda's gaming scene.",
+  openGraph: {
+    title: 'eGamers.rw - Funky Monkey Arcade in Mundi Center Rwandex,',
+    description: "Welcome to eGamers.rw! Explore the ultimate gaming experience at Funky Monkey Arcade, located in Mundi Center Rwandex, Rwanda. Enjoy a wide range of arcade games and entertainment for all ages. Join us for a fun-filled adventure in the heart of Rwanda's gaming scene.",
+    url: 'https://www.egamers.rw',
+    siteName: 'eGamers',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/gaming-906ed.appspot.com/o/image.jpg?alt=media&token=04f8cca6-e7bb-4e30-8249-f4d89140ccdf',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  icons: {
+    icon: "https://egamers.rw/favicon.ico"
+  }
 }
 
 export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:url" content="https://www.egamers.rw" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="eGamers.rw - Funky Monkey Arcade in Mundi Center Rwandex," />
-        <meta property="og:description" content="Welcome to eGamers.rw! Explore the ultimate gaming experience at Funky Monkey Arcade, located in Mundi Center Rwandex, Rwanda. Enjoy a wide range of arcade games and entertainment for all ages. Join us for a fun-filled adventure in the heart of Rwanda's gaming scene." />
-        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/gaming-906ed.appspot.com/o/image.jpg?alt=media&token=04f8cca6-e7bb-4e30-8249-f4d89140ccdf" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="egamers.rw" />
-        <meta property="twitter:url" content="https://www.egamers.rw" />
-        <meta name="twitter:title" content="eGamers.rw - Funky Monkey Arcade in Mundi Center Rwandex," />
-        <meta name="twitter:description" content="Welcome to eGamers.rw! Explore the ultimate gaming experience at Funky Monkey Arcade, located in Mundi Center Rwandex, Rwanda. Enjoy a wide range of arcade games and entertainment for all ages. Join us for a fun-filled adventure in the heart of Rwanda's gaming scene." />
-        <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/gaming-906ed.appspot.com/o/image.jpg?alt=media&token=04f8cca6-e7bb-4e30-8249-f4d89140ccdf" />
-
-        <link rel="icon" href="https://www.egamers.rw/favicon.ico" type="image/x-icon"></link>
-        <link rel="shortcut icon" href="https://www.egamers.rw/favicon.ico" type="image/x-icon"></link>
-      </Head>
       <body className='bg-primary'>
         <Header />
           {children}
